7b1fcef2047ff6f4b2a15373ac30387fe4727e06
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-24 20:22:51 +0000

    flame-user 10 QD1A.190821.011.C4 5917693 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,22 @@
+DEVICE_PATH := device/google/flame
+BOARD_VENDOR := google
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-10-05
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.atrace@1.0-service.pixel.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.1-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.gnss@2.0-service-qti.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.power@1.3-service.pixel.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.thermal@2.0-service.pixel.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.vibrator@1.3-service.coral.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_android.hardware.health.storage@1.0.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_face.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_input.classifier.floral.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_wifi_ext.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/google/flame/BoardConfigVendor.mk
